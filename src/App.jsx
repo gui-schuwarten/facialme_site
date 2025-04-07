e6971beceecc50e40192ae774a46bda5a4e33928
Guilherme Schuwarten <root@GFT-6zSe4cGrFbV.gft.com>
2025-04-07 19:23:59 -0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
 import AboutSection from './components/About/AboutSection';
 import SpecialtiesSection from './components/Specialties/SpecialtiesSection';
-import SpecialtiesSection_v2 from './components/Specialties/SpecialtiesSection_v2';
 import ContactSection from './components/Contact/ContactSection';
 
 function App() {
